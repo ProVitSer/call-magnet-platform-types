3383b0f1756fb6048b2f767bffe3f51b247873ef
ProVitSer <prokinvs@gmail.com>
2023-12-01 12:02:52 +0000

    feat: add notification types

---
+++ notification/interfaces.ts
@@ -0,0 +1,33 @@
+import { AvatarType, NotificationType } from './types';
+
+export interface Notification {
+    clientId: string;
+    type: NotificationType;
+    avatarType: AvatarType;
+    avatar: string;
+    title: string;
+    smalText: string;
+    html: string;
+    link?: string;
+    isRead: boolean;
+    author: NotificationAuthor;
+    isDeleted: boolean;
+    createdAt: string;
+}
+
+export class NotificationAuthor {
+    firstName: string;
+    lastName: string;
+}
+
+export interface AddNotificationData {
+    clientId: string;
+    type?: NotificationType;
+    avatarType?: AvatarType;
+    avatar?: string;
+    title: string;
+    smalText: string;
+    html: string;
+    link?: string;
+    author?: NotificationAuthor;
+}
