4db384fcac563cf309e05edb577f03e1d4bc990a
ProVitSer <prokinvs@gmail.com>
2023-12-09 15:46:17 +0000

    add sms types

---
+++ sms/types.ts
@@ -0,0 +1,14 @@
+export enum Role {
+    api = 'api',
+    analitic = 'analitic',
+    crm = 'crm',
+    autoDial = 'auto-dial',
+    sms = 'sms',
+    telegram = 'telegram',
+    kpi = 'kpi',
+}
+
+export enum Status {
+    active = 'active',
+    deactivated = 'deactivated',
+}
